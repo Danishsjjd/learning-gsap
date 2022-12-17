78107e8ff1afc52c45cb0c60bc9869645f3fa2ff
DanishSjjd <danishsjjad@gmail.com>
2022-12-17 11:40:25 +0500

    feat: gsap flip-example

--- src/flip.ts
+++ src/flip.ts
@@ -1,10 +1,30 @@
-import { gsap } from "gsap";
+import { Flip } from "gsap/Flip";
+import { gsap, Power1 } from "gsap";
+
+gsap.registerPlugin(Flip);
 
 const btn = document.getElementById("changeBtn") as HTMLButtonElement;
 const container = document.querySelector(".container");
 const newContainer = document.querySelector(".newContainer");
-const box = document.getElementById("box");
+const box = document.getElementById("box") as HTMLElement;
 
 btn.onclick = () => {
-  (box?.parentNode === container ? newContainer : container)?.append(box);
+  const tl = gsap.to(box, {
+    scale: 2,
+    transform: "transform(-50%, -50%)",
+    duration: 0.5,
+    onComplete: function () {
+      const state = Flip.getState(box);
+      (box?.parentNode === container ? newContainer : container)?.append(box);
+      Flip.from(state, {
+        ease: Power1.easeInOut,
+        duration: 0.5,
+        scale: false,
+        onComplete: () => {
+          tl.reverse();
+        },
+      });
+      console.log("completed");
+    },
+  });
 };
